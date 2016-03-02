58f0ab90d71e20b5b700338f9a92a2517e581283
yang1223 <whitesheep1223@gmail.com>
2016-03-03 02:03:41 +0800

    get appData path

--- src/ZhTranslate.java
+++ src/ZhTranslate.java
@@ -6,28 +6,39 @@ import java.io.File;
 public class ZhTranslate {
     public static void main(String[] args){
 
-        String haroopadPath = "C:\\Users\\" + System.getProperty("user.name")
-                + "\\AppData\\Roaming\\Haroo Studio\\Haroopad";
+        String haroopadPath = getAppDataPath() + "\\Haroo Studio\\Haroopad";
+        File haroopadDir = new File(haroopadPath);
+        if(haroopadDir.isDirectory()){
+            String zhPath = haroopadPath+"\\Libraries\\.locales\\zh";
+            File zh = new File(zhPath);
+            if(zh.isDirectory()){
+                String[] fileList = zh.list();
+                for(String filename:fileList){
+                    if(filename.endsWith(".json")){
+                        // rename the original json files
+                        File file1 = new File(zh.getPath(),filename);
+                        File file2 = new File(zh.getPath(),"old_"+filename);
+                        file1.renameTo(file2);
+                    }
+                }
+                // TODO : copy 4 .json files to here
+            }
+            System.out.println(zh.isDirectory());
+        }
 
-
-        int a = 3;
-//        System.out.println(1+(a++)-1);
-//        System.out.println(a++);
-        ++a;
-        System.out.println(a);
-
-        if(a==4) System.out.println("a=4");
-
-
-
-//        File file = new File(haroopadPath);
-//        System.out.println(file.getPath());
-//
-//        System.out.println("user");
-//
-//        System.out.println(System.getProperty("os.name"));
-//        System.out.println(System.getProperty("user.name"));
-//
-//        System.out.println(args.length==0);
     }
+
+    public static String getAppDataPath(){
+        String OS = System.getProperty("os.name").toLowerCase();
+        if(OS.contains("win")){
+            String appData = System.getenv("APPDATA");
+            if(appData!=null) return appData;
+            else return System.getProperty("user.home")+"\\AppData\\Roaming";
+        } else if(OS.contains("mac")){
+            return System.getProperty("user.home") + "\\Library\\Application Support";
+        }
+        return System.getProperty("user.home");
+
+    }
+
 }
