f9ad47af35072f1a6aae752813e238a7bcb19ae2
yang1223 <whitesheep1223@gmail.com>
2016-03-05 03:06:09 +0800

    almost done!

--- src/ZhTranslate.java
+++ src/ZhTranslate.java
@@ -1,4 +1,6 @@
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Created by cavitation on 2016/3/2.
@@ -6,24 +8,36 @@ import java.io.File;
 public class ZhTranslate {
     public static void main(String[] args){
 
-        String haroopadPath = getAppDataPath() + "\\Haroo Studio\\Haroopad";
-        File haroopadDir = new File(haroopadPath);
-        if(haroopadDir.isDirectory()){
-            String zhPath = haroopadPath+"\\Libraries\\.locales\\zh";
-            File zh = new File(zhPath);
-            if(zh.isDirectory()){
-                String[] fileList = zh.list();
-                for(String filename:fileList){
-                    if(filename.endsWith(".json")){
+        final String[] jsonFiles = {"error.json" , "menu.json" , "pad.json" , "preference.json"};
+        final String[] _jsonFiles = {"_error.json" , "_menu.json" , "_pad.json" , "_preference.json"};
+
+        try{
+            String haroopadPath = getAppDataPath() + "\\Haroo Studio\\Haroopad";
+
+            File haroopadDir = new File(haroopadPath);
+            if(haroopadDir.isDirectory()){
+                String zhPath = haroopadPath+"\\Libraries\\.locales\\zh";
+                File zh = new File(zhPath);
+                if(zh.isDirectory()){
+                    for(int i = 0 ; i < jsonFiles.length ; i++){
                         // rename the original json files
-                        File file1 = new File(zh.getPath(),filename);
-                        File file2 = new File(zh.getPath(),"old_"+filename);
-                        file1.renameTo(file2);
+                        File file1 = new File(zh.getPath(),jsonFiles[i]);
+                        File file2 = new File(zh.getPath(),_jsonFiles[i]);
+                        if(file2.exists())
+                            file1.delete();
+                        else
+                            file1.renameTo(file2);
+
+                        File source = new File("zh-tw",jsonFiles[i]);
+                        File target = new File(zh.getPath(),jsonFiles[i]);
+                        Files.copy(source.toPath(),target.toPath());
+
                     }
                 }
-                // TODO : copy 4 .json files to here
             }
-            System.out.println(zh.isDirectory());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
 
     }
@@ -38,7 +52,8 @@ public class ZhTranslate {
             return System.getProperty("user.home") + "\\Library\\Application Support";
         }
         return System.getProperty("user.home");
-
     }
+
+
 
 }
