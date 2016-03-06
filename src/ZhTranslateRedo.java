f2439934b50a282827b9d270730ba98335352b81
Yang, Chi-Chang <whitesheep1223@gmail.com>
2016-03-06 19:43:24 +0800

    add Redo

--- src/ZhTranslateRedo.java
+++ src/ZhTranslateRedo.java
@@ -1,16 +1,13 @@
-import java.io.*;
+import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 /**
- * Created by cavitation on 2016/3/2.
+ * Created by Yang on 2016/3/6.
  */
-public class ZhTranslate {
+public class ZhTranslateRedo {
     public static void main(String[] args){
-
         final String[] jsonFiles = {"error.json" , "menu.json" , "pad.json" , "preference.json"};
         final String[] _jsonFiles = {"_error.json" , "_menu.json" , "_pad.json" , "_preference.json"};
-
 
         try{
             String haroopadPath = getHaroopadPath(args);
@@ -21,20 +18,12 @@ public class ZhTranslate {
                 File zh = new File(zhPath);
                 if(zh.isDirectory()){
                     for(int i = 0 ; i < jsonFiles.length ; i++){
-
-                        // rename the original json files
-                        File file1 = new File(zh.getPath(),jsonFiles[i]);
-                        File file2 = new File(zh.getPath(),_jsonFiles[i]);
-                        if(file2.exists())
-                            file1.delete();
-                        else
-                            file1.renameTo(file2);
-                        File source = new File("zh-tw",jsonFiles[i]);
-                        File target = new File(zh.getPath(),jsonFiles[i]);
-                        Files.copy(source.toPath(), target.toPath());
-                        System.out.println("translating......" + (25 + i * 25) + "%");
+                        File file1 = new File(zh.getPath(),_jsonFiles[i]);
+                        File file2 = new File(zh.getPath(),jsonFiles[i]);
+                        file2.delete();
+                        file1.renameTo(file2);
                     }
-                    System.out.println("Finish!");
+                    System.out.println("Redo!");
                 } else {
                     throw new HaroopadNotFoundException("Cannot find language folder! Please check the path!");
                 }
@@ -65,15 +54,15 @@ public class ZhTranslate {
     }
 
     public static String getAppDataPath() {
-        String OS = System.getProperty("os.name").toLowerCase();
-        if(OS.contains("win")){
-            String appData = System.getenv("APPDATA");
-            if(appData!=null) return appData;
-            else return System.getProperty("user.home")+"\\AppData\\Roaming";
-        } else if(OS.contains("mac")){
-            return System.getProperty("user.home") + "\\Library\\Application Support";
-        }
-        return System.getProperty("user.home");
+            String OS = System.getProperty("os.name").toLowerCase();
+            if(OS.contains("win")){
+                String appData = System.getenv("APPDATA");
+                if(appData!=null) return appData;
+                else return System.getProperty("user.home")+"\\AppData\\Roaming";
+            } else if(OS.contains("mac")){
+                return System.getProperty("user.home") + "\\Library\\Application Support";
+            }
+            return System.getProperty("user.home");
     }
 
     static class HaroopadNotFoundException extends Exception {
@@ -81,5 +70,4 @@ public class ZhTranslate {
             super(message);
         }
     }
-
 }
